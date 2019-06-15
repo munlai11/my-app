8ec3ec85b5613139e4772b7fd5c3c2df7dc4c62e
Lai <alex.lai@atos.net>
2019-06-15 19:49:35 +0100

    bootstrap

--- posts/views.py
+++ posts/views.py
@@ -8,11 +8,11 @@ from django.contrib.auth.decorators import login_required
 def post_list(request):
     #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.all()
-    return render(request, 'posts/post_list.html', {'posts': posts})
+    return render(request, 'posts/index.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'posts/post_detail.html', {'post': post})
+    return render(request, 'posts/index.html', {'post': post})
 
 @login_required
 def post_new(request):
@@ -26,7 +26,7 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'posts/post_edit.html', {'form': form})
+    return render(request, 'posts/index.html', {'form': form})
 
 @login_required
 def post_edit(request, pk):
@@ -41,12 +41,12 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'posts/post_edit.html', {'form': form})
+    return render(request, 'posts/index.html', {'form': form})
 
 @login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'posts/post_draft_list.html', {'posts': posts})
+    return render(request, 'posts/index.html', {'posts': posts})
 
 @login_required
 def post_publish(request, pk):
@@ -72,7 +72,7 @@ def add_comment_to_post(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request, 'posts/add_comment_to_post.html', {'form': form})
+    return render(request, 'posts/index.html', {'form': form})
 
 @login_required
 def comment_approve(request, pk):
